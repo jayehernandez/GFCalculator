82d83921b0f1fb01d1484deef060d7cb4b885ab2
Jaye Hernandez <hernandez.jaye@gmail.com>
2017-05-23 22:03:47 +0800

    Added print function

--- proj.py
+++ proj.py
@@ -1,14 +1,25 @@
-
+def printPoly(arr):
+	x = len(arr)
+	x = x - 1
+	for i in arr:
+		if x == 0:
+			print str(i)
+		else:
+			print str(i) + "x^" + str(x) + " + ",
+			x = x - 1
+	return " "
 
 Ax = raw_input("Input A(x): ")
 Bx = raw_input("Input B(x): ")
-Px = raw_input("Input P(x): ")
+# Px = raw_input("Input P(x): ")
 
 Ax = Ax.split(" ")
 Bx = Bx.split(" ")
 
-for i in Ax:
-	print i
-	print Ax.index(i)
+# printing
+print "A(x): ", printPoly(Ax)
+print "B(x): ", printPoly(Bx)
 
-print Ax
+# for i in range(max(len(Ax), len(Bx))):
+	# print Ax[i]
+	# print Bx[i]
