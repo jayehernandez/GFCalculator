3532f94ca9aac777d2a5e814b14eb16f582a56f3
Jaye Hernandez <hernandez.jaye@gmail.com>
2017-05-23 21:42:24 +0800

    Initial

--- proj.py
+++ proj.py
@@ -1,7 +1,14 @@
-A = raw_input("Input A(x): ")
-B = raw_input("Input B(x): ")
-P = raw_input("Input P(x): ")
 
-Ax = A.split(" ")
+
+Ax = raw_input("Input A(x): ")
+Bx = raw_input("Input B(x): ")
+Px = raw_input("Input P(x): ")
+
+Ax = Ax.split(" ")
+Bx = Bx.split(" ")
+
+for i in Ax:
+	print i
+	print Ax.index(i)
 
 print Ax
