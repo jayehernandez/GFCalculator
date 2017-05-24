f21cbf9e387f1ca18813626c8c1c2915db0647eb
Jaye Hernandez <hernandez.jaye@gmail.com>
2017-05-25 02:54:53 +0800

    Added mul and div, no computations yet

--- gfcalcu.py
+++ gfcalcu.py
@@ -1,31 +1,90 @@
-def addLeadZeroes(arr, a, b):
-	for i in range(a - b):
+def addLeadZeroes(arr, a):
+	for x in range(a - len(arr)):
 		arr.insert(0,0)	
+
+def delLeadZeroes(arr):
+	while (arr[0] == 0 and len(arr) != 1):
+		arr.remove(0)
+
+def addEndZeroes(arr, a):
+	for x in range(a - len(arr)):
+		arr.append(0)
 
 def printPoly(arr):
 	x = len(arr)
 	x = x - 1
 	for i in arr:
-		if x == 0:
+		if i == 0:
+			x -= 1 
+		elif x == 0:
 			print str(i)
 		elif x == 1:
 			print str(i) + "x + ",
-			x = x - 1
+			x -= 1
 		else:
 			print str(i) + "x^" + str(x) + " + ",
-			x = x - 1
+			x -= 1
 	return " "
 
 def addOrSub(a, b):
 	if len(a) > len(b):
-		addLeadZeroes(b, len(a), len(b))
+		addLeadZeroes(b, len(a))
 	elif len(a) < len(b):
-		addLeadZeroes(a, len(b), len(a))
-
+		addLeadZeroes(a, len(b))
 	out = []
 	for i in range(len(a)):
 		out.append(a[i] ^ b[i])
-	printPoly(out)
+	return out
+
+def binMul(a, b, p):
+	a = [int(x) for x in bin(a)[2:]]
+	b = [int(x) for x in bin(b)[2:]]
+
+	out = []
+	for x in range(len(b)-1, -1, -1):
+		toAdd = []
+		for y in range(len(a)):
+			toAdd.append(a[y]*b[x])
+		for y in range((len(b)-1)-x):
+			toAdd.append(0)
+		addLeadZeroes(out, len(toAdd))
+		out = addOrSub(out, toAdd)
+	delLeadZeroes(out)
+	# need to mod p if > m - 1
+	p1 = list(p)
+	while(len(out) > len(p1)-1):
+		addEndZeroes(p1, len(out))
+		out = addOrSub(out, p1)
+		delLeadZeroes(out)
+	return int(''.join(map(str,out)),2)
+
+def mul(a, b, p):
+	out = []
+	for x in range(len(b)-1, -1, -1):
+		toAdd = []
+		for y in range(len(a)):
+			toAdd.append(binMul(a[y],b[x],p))
+		for y in range((len(b)-1)-x):
+			toAdd.append(0) # append sa dulo
+		addLeadZeroes(out, len(toAdd))
+		out = addOrSub(out, toAdd)
+	delLeadZeroes(out)
+	return out
+
+def div(a, b, p):
+	out = []
+	dividend = int(''.join(map(str,a)))
+	divisor = int(''.join(map(str,b)))
+	while (dividend > divisor):
+		x = a[0]/b[0]
+		out.append(x)
+		prod = mul(b, [x], p)
+		addEndZeroes(prod, len(a))
+		a = addOrSub(a, prod)
+		delLeadZeroes(a)
+		dividend = int(''.join(map(str,a)))
+		divisor = int(''.join(map(str,b)))
+	return out, a
 
 Ax = raw_input("Input A(x): ")
 Bx = raw_input("Input B(x): ")
@@ -33,6 +92,7 @@ Px = raw_input("Input P(x): ")
 
 Ax = [int(x) for x in Ax.split(" ")]
 Bx = [int(x) for x in Bx.split(" ")]
+Px = [int(x) for x in Px.split(" ")]
 
 print """
 Four operations that could be done:
@@ -47,6 +107,13 @@ op = input("Pick an operation to perform (Input 1, 2, 3, or 4): ")
 # printing
 print "A(x): ", printPoly(Ax)
 print "B(x): ", printPoly(Bx)
+print "P(x): ", printPoly(Px)
 
 if op == 1 or op == 2:
-	addOrSub(Ax, Bx)
+	printPoly(addOrSub(Ax, Bx))
+elif op == 3:
+	printPoly(mul(Ax, Bx, Px))
+elif op == 4:
+	quo, rem = div(Ax, Bx, Px)
+	printPoly(quo)
+	printPoly(rem)
